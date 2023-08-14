af326524470f1974627912d8b63c0219d44ba312
yasaveevruslan <yasaveev.ruslan.2113@mail.ru>
2023-08-14 14:40:29 +0300

    add limit cort

--- src/main/java/com/example/demo2/training/Elements.java
+++ src/main/java/com/example/demo2/training/Elements.java
@@ -8,7 +8,8 @@ public class Elements
     public static float coordinatesX, coordinatesY, coordinatesZ = 0;
 
     public static float positionRobotX = 290, positionRobotY = 290, positionRobotZ = 0;
-    public static float lastCoordinatesX, lastCoordinatesY, lastCoordinatesZ = 0;
+
+    public static int fronteirLeft = 50, fronteirRight = 1043, fronteirUp = 70, fronteirDown = 570;
 
     public void setAxisSpeed(float x, float y, float z)
     {
@@ -33,7 +34,6 @@ public class Elements
         coordinatesY += speedY * 4;
         coordinatesZ += speedZ;
 
-//        System.out.println("coordinatesX: " + coordinatesX + " coordinatesY: " + coordinatesY + " coordinatesZ: " + coordinatesZ);
     }
 
     public void resetCoordinates(float x, float y, float z)
@@ -43,20 +43,53 @@ public class Elements
         coordinatesZ = z;
     }
 
-    public void changePosition()
-    {
-//        positionRobotX += coordinatesX / 4 - lastCoordinatesX;
-//        lastCoordinatesX = coordinatesX / 4;
-//
-//        positionRobotY += coordinatesY / 4 - lastCoordinatesY;
-//        lastCoordinatesY = coordinatesY / 4;
-
-        positionRobotX += speedX;
-
-        positionRobotY += speedY;
+    public void changePosition() {
 
         positionRobotZ += speedZ;
 
-//        System.out.println("positionRobotX: " + positionRobotX + " positionRobotY: " + positionRobotY + " positionRobotZ: " + positionRobotZ);
+        int frontierX = 0;
+        int frontierY = 0;
+
+        if (Function.InRangeBool(Math.abs(positionRobotZ), 0, 20) || Function.InRangeBool(Math.abs(positionRobotZ), 160, 200))
+        {
+            frontierX = 105;
+            frontierY = 100;
+        }
+        else if (Function.InRangeBool(Math.abs(positionRobotZ), 70, 110) || Function.InRangeBool(Math.abs(positionRobotZ), 250, 290))
+        {
+            frontierX = 100;
+            frontierY = 105;
+        }
+        else
+        {
+            frontierX = 110;
+            frontierY = 110;
+        }
+
+        if ((positionRobotX <= fronteirLeft && speedX < 0))
+        {
+            positionRobotX = fronteirLeft;
+        }
+        else if ((positionRobotX >= fronteirRight - frontierX && speedX > 0))
+        {
+            positionRobotX = fronteirRight - frontierX;
+        }
+        else
+        {
+            positionRobotX += speedX;
+        }
+
+        if ((positionRobotY <= fronteirUp && speedY < 0))
+        {
+            positionRobotY = fronteirUp;
+        }
+        else if (positionRobotY >= fronteirDown - frontierY && speedY > 0)
+        {
+            positionRobotY = fronteirDown - frontierY;
+        }
+        else
+        {
+            positionRobotY += speedY;
+        }
     }
 }
--- src/main/java/com/example/demo2/training/States.java
+++ src/main/java/com/example/demo2/training/States.java
@@ -9,10 +9,10 @@ public class States {
     public static IState[][] states = new IState[][]{
             {
                     new Start(),
-                    new Odometry(States.ABSOLUTE_ODOMETRY, 1000, 0, 90, SmoothEnum.OFF),
-                    new Odometry(States.ABSOLUTE_ODOMETRY, 1000, 1000, 180, SmoothEnum.OFF),
+                    new Odometry(States.ABSOLUTE_ODOMETRY, 4000, 0, 90, SmoothEnum.OFF),
+                    new Odometry(States.ABSOLUTE_ODOMETRY, 3500, 1000, 180, SmoothEnum.OFF),
                     new Odometry(States.ABSOLUTE_ODOMETRY, 0, 1000, -90, SmoothEnum.OFF),
-                    new Odometry(States.ABSOLUTE_ODOMETRY, 0, 0, 0, SmoothEnum.OFF),
+                    new Odometry(States.ABSOLUTE_ODOMETRY, 0, -1000, 0, SmoothEnum.OFF),
                     new End(),
             }
     };
